3467bdf3e7c9a96fb4c7490f2e75b203022d7e04
Oskar Codes <oskarbertrand@icloud.com>
2019-06-09 20:49:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ if (commands[0] == "help") {
 log.innerHTML +=
 '<p style="color: ' +
       color +
-"\">Command list:<br>help: shows this help panel.<br>info: shows information about this console.<br>clear: clears the console<br>codepen [pen id] [view]: browses to codepen, at the specified pen id, with the specified view (pen, details, full, debug).<br>codepenuser [user]: browses to the specified user's codepen profile.<br>codepensearch [query]: searches Codepen for your query.<br>url [url]: browses to the specified url.<br>google [query]: searches Google for your query.<br>youtube [query]: searches YouTube for your query.<br>print [text]: prints text to the console.<br>color [value]: sets the color of the console's text, or resets it if no color is specified.<br>js [code]: executes some JavaScript code.<br></p>";
+"\">Command list:<br>help: shows this help panel.<br>info: shows information about this console.<br>clear: clears the console<br>codepen [pen id] [view]: browses to codepen, at the specified pen id, with the specified view (pen, details, full, debug).<br>codepenuser [user]: browses to the specified user's codepen profile.<br>codepensearch [query]: searches Codepen for your query.<br>url [url]: browses to the specified url.<br>cache [url] [view]: browses to the cache of the specified url, and on the specified view.(full,text,source)<br>google [query]: searches Google for your query.<br>youtube [query]: searches YouTube for your query.<br>print [text]: prints text to the console.<br>color [value]: sets the color of the console's text, or resets it if no color is specified.<br>js [code]: executes some JavaScript code.<br></p>";
   } else if (commands[0] == "info") {
 log.innerHTML += "<p style='color: " + color + "'>This console was made by ZOSK, a young amateur web and game developer. It was originally made in Codepen, and then moved to Github. That's why a lot of commands have to do with Codepen.<br>Check out my Twitter account <a style='color: "+ color +";' href='https://twitter.com/ZOSK17'>here</a>.</p><br>";
   } else if (commands[0] == "codepen") {
@@ -41,6 +41,21 @@ window.open(commands[1]);
 log.innerHTML +=
 "<p style='color: red'>Error: No url specified.</p><br>";
     }
+  } else if (commands[0] == "cache") {
+if (commands[1]) {
+      if (commands[2] == "full") {
+            window.open("http://webcache.googleusercontent.com/search?q=cache:"+ commands[1] +"&safe=active&client=safari&hl=en-ch&prmd=ivn&strip=0&vwsrc=0");
+      } else if (commands[2] == "text") {
+            window.open("http://webcache.googleusercontent.com/search?q=cache:"+ commands[1] +"&safe=active&client=safari&hl=en-ch&prmd=ivn&strip=1&vwsrc=0");
+      } else if (commands[2] == "source") {
+            window.open("http://webcache.googleusercontent.com/search?q=cache:"+ commands[1] +"&safe=active&client=safari&hl=en-ch&prmd=ivn&strip=0&vwsrc=1");
+      } else {
+            window.open("http://webcache.googleusercontent.com/search?q=cache:"+ commands[1] +"&safe=active&client=safari&hl=en-ch&prmd=ivn&strip=0&vwsrc=0");
+      }
+} else {
+      log.innerHTML +=
+"<p style='color: red'>Error: No url specified.</p><br>";
+}
   } else if (commands[0] == "google") {
 if (commands[1]) {
 window.open("https://google.com/search?q=" + encodeURIComponent(parameters));
