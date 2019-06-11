16edc4fd7df70e69aba900031b363b8362717624
Oskar Codes <oskarbertrand@icloud.com>
2019-06-11 18:57:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var input = document.querySelector("#input");
 var log = document.querySelector(".log");
 var color = "#22ff11";
 log.innerHTML =
-  "<p>Welcome to this little command line ! Start by typing help</p><br>";
+  "<p>Welcome to this little command line ! Start by typing help.</p><br>";
 
 function submit() {
   var command = input.value;
